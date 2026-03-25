03c7e636d6d276d708be68cd3c6155aa67958aea
HenriIncerti <henrique_incerti@estudante.sesisenai.org.br>
2026-03-25 11:52:22 -0300

    add - Atividade 4

---
+++ scripts/ex04.js
@@ -0,0 +1,21 @@
+function ordenacaoValoresInteiros() {
+    let numeros = [];
+
+    let base;
+
+    for(let i = 0; i < 4; i++){
+        numeros[i] = prompt("Informe um valor para ordenação: ");
+    }
+
+    for(let i = 0; i < 4; i++){
+        for(let j = 0; j < 4; j++){
+            if(numeros[j] < numeros[j+1]){
+                base = numeros[j];
+                numeros[j] = numeros[j+1];
+                numeros[j+1] = base;
+            }
+        }
+    }
+    alert("A ordem decrescente é: " + numeros[0] + " " + numeros[1] + " " + numeros[2] + " " + numeros[3]);
+}
+
